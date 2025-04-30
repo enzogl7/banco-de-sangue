0785667e88b23e7c0ad14eafa1936326f3e0d66e
Enzo Lima <enzolima527@gmail.com>
2025-04-30 13:43:20 -0300

    Ajuste migration/tabela

--- src/main/resources/db/migration/V1__criar_tabelas.sql
+++ src/main/resources/db/migration/V1__criar_tabelas.sql
@@ -1,1 +1,11 @@
-CREATE TABLE tipos_sanguineos (id SERIAL PRIMARY KEY, tipo VARCHAR(3) NOT NULL);
+CREATE TABLE tipos_sanguineos (id SERIAL PRIMARY KEY,tipo VARCHAR(3) NOT NULL);
+
+CREATE TABLE doacoes_possiveis ( doador_id BIGINT NOT NULL, receptor_id BIGINT NOT NULL, PRIMARY KEY (doador_id, receptor_id),
+                                 FOREIGN KEY (doador_id) REFERENCES tipos_sanguineos(id),
+                                 FOREIGN KEY (receptor_id) REFERENCES tipos_sanguineos(id));
+
+CREATE TABLE recebimentos_possiveis (receptor_id BIGINT NOT NULL,doador_id BIGINT NOT NULL,
+                                        PRIMARY KEY (receptor_id, doador_id),
+                                        FOREIGN KEY (receptor_id) REFERENCES tipos_sanguineos(id),
+                                        FOREIGN KEY (doador_id) REFERENCES tipos_sanguineos(id)
+);
